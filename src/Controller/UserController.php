83a35d8be0eea478909a898e8dd9741616c590de
Atit <you@example.com>
2023-05-24 00:01:59 +0100

    [LEX-TEST] User controller is for testing only not part of the test

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,13 +14,14 @@ class UserController extends AbstractController
     #[Route('/user', name: 'app_user')]
     public function index(UserService $userService): JsonResponse
     {
+        // this is for testing and checking the on page
         // $user = $userService->createUser('atit@email.com', 'password', 'atit');
-        // $users = $userService->getUsers();
-        $user = $userService->getUserByUsername('atit1');
+        $users = $userService->getUsers();
+        // $user = $userService->getUserByUsername('atit1');
         // $validateUser = $userService->validateUserCredential('atit', 'password');
         // $updateUser = $userService->updateUserCredential('atit', 'password', 'password1');
 
-        return $this->json($user, Response::HTTP_OK, [], [
+        return $this->json($users, Response::HTTP_OK, [], [
            ObjectNormalizer::SKIP_NULL_VALUES => true
         ]);
     }
